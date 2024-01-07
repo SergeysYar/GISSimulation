fe7af0f3c913d4194aa27ebd37735b4ae87147c5
SergeysYar <60104925+SergeysYar@users.noreply.github.com>
2024-01-07 17:00:14 +0300

    C#

---
+++ GiSSim/GiSSim/FindingShortestPath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickGraph.Algorithms.ShortestPath;
+
+namespace GiSSim
+{
+    internal class FindingShortestPath
+    {
+        // Поиск кратчайшего пути между двумя узлами
+        public void byTime ()
+        { }
+        public void byDistance()
+        { }
+    }
+}
---
+++ GiSSim/GiSSim/RoadEdge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickGraph;
+
+namespace GiSSim
+{
+    internal class RoadEdge
+    {
+        public int Id;
+    }
+}
